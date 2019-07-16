2715d39e539ea6d23103009276f6a1a6b4b829e0
Vrushabh <sirsam.vs@gmail.com>
2019-07-16 12:14:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vrushabh/onlongclick/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.vrushabh.onlongclick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+    }
+}
